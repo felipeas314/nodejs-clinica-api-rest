87760fe258690e670ecd680ba7d23921f4a65253
Felipe <felipeas314@gmail.com>
2020-12-14 20:08:21 -0300

    criação do middleware de validação do token

--- src/app/middlewares/token-middleware.js
+++ src/app/middlewares/token-middleware.js
@@ -1,4 +1,28 @@
-module.exports = (req, res, next) => {
+const { verifyToken } = require('../services/token-service');
+
+module.exports = async (req, res, next) => {
   console.log('Middleware de token');
+
+  let authorizationHeader = req.headers['authorization'];
+  
+  if(!authorizationHeader){
+    return res.status(401).json({
+      message: 'Token é obrigatório',
+      date: new Date()
+    })
+  }
+
+  const token = authorizationHeader.slice(7,authorizationHeader.length);
+
+  try{
+    await verifyToken(token);
+  }catch(err){
+    console.log(err);
+    return res.status(401).json({
+      message: 'Token inválido',
+      date: new Date()
+    })
+  }
+
   next();
 }
